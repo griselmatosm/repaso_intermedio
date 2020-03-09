0c119b2f7415ed8024f340d915f6e3de7c664208
Grisel Matos <griselmatosm@gmail.com>
2020-03-09 21:09:45 +0100

    Finished interections

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,40 @@
 'use strict';
+//data
 
-console.log('>> Ready :)');
+const face = document.querySelector('.js-face');
+const selectFace = document.querySelector('.js-select');
+const container = document.querySelector('.js-container');
+const button = document.querySelector('.js-button');
+
+//Función manejadora de eventos
+
+function handleButtonUpdate(){
+  selectFaceValue();
+  changeBackground();
+}
+
+//listener button
+
+button.addEventListener('click', handleButtonUpdate);
+
+// Seleccionar carita
+function selectFaceValue(){
+  face.innerHTML = selectFace.value;
+}
+
+//Generar número aleatorio
+function getRandomNumber(){
+  return Math.floor(Math.random()*100);
+}
+
+
+//Intercambiar color de fondo
+function changeBackground(){
+  const randomNumber = getRandomNumber();
+  if (randomNumber % 2  === 0) {
+    container.classList.add('correct-yellow-color');
+  } else {
+    container.classList.remove('correct-yellow-color');
+  }
+}
+
